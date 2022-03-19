c329cbffa94eb46e6e1089297742ebba8ae72009
DonixZ <dannyzaletsky@gmail.com>
2022-03-19 16:04:04 +0200

    java homework for school

---
+++ src/Employee.java
@@ -0,0 +1,40 @@
+public class Employee {
+
+    //Data
+    String firstName, lastName;
+    int id, floor;
+    double salary;
+
+    //Func
+    public Employee(String firstName, String lastName, int id, int floor, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.floor = floor;
+        this.salary = salary;
+    }
+
+    public void raiseUpSalary(double Salary) {
+        this.salary = Salary;
+    }
+
+    public void changeFloor(int newFloor) {
+        this.floor = newFloor;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                ", floor=" + floor +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee Daniel = new Employee("Daniel",
+                "Zaletsky",
+                315610915,
+                3,
+                5.3);
+        Employee Itzik = new Employee("Itzik",
+                "Haim",
+                315610907,
+                2,
+                5.3);
+        Employee Gabi = new Employee("Gabi",
+                "Zaletsky",
+                315610907,
+                4,
+                5.3);
+
+
+
+
+    }
+}
+
+
